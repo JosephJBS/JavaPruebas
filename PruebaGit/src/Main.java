6e01e214c68cca6ffe310eada6b0caf0b8e4940f
JosephBS <jjbs1805@gmail.com>
2022-04-10 22:43:07 -0500

    add new text to see commit list

--- PruebaGit/src/Main.java
+++ PruebaGit/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("Nueva Modificacion a master");
+        System.out.println("Mostrar lista commits");
     }
 }
